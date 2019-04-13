a79143b6806ab4ff1202ef9895ef25f3512cd738
Naman Gera <43007653+NamanNimmo10@users.noreply.github.com>
2019-04-13 19:28:55 +0530

    Update pe041.py

--- pe041.py
+++ pe041.py
@@ -6,7 +6,7 @@ from collections import Counter
 def isPrime(n) : 
     if (n <= 1) : 
         return False                #this is my brute force approach, if  I see a better solutionin the PE forum, will edit this :)
-    if (n <= 3) : 
+    if (n <= 3) :                   # EDIT: A much better approach: https://www.xarg.org/puzzle/project-euler/problem-41/
         return True
     if (n % 2 == 0 or n % 3 == 0) : 
         return False
