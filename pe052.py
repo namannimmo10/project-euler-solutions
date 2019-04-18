1b95335651d81cef6bba9121c980d0041462e35e
Naman Gera <43007653+NamanNimmo10@users.noreply.github.com>
2019-04-18 18:48:54 +0530

    Update pe052.py

--- pe052.py
+++ pe052.py
@@ -3,7 +3,7 @@
 
 import time
 start = time.time()
-for i in range(1,1000000): #bruuuuuute force 
+for i in range(101,1000000): #bruuuuuute force 
   if set(list(str(i))) == set(list(str(2*i))) == set(list(str(3*i))) == set(list(str(4*i))) == set(list(str(5*i))) == set(list(str(6*i))):
     print(i)
     break
