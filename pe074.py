3cee80ed907ac06a30452b9c5ef6da545dd17f0d
Naman Gera <43007653+NamanNimmo10@users.noreply.github.com>
2019-05-02 00:31:42 +0530

    Create pe074.py

---
+++ pe074.py
@@ -0,0 +1,16 @@
+#code created by NamanNimmo Gera
+#12:28am, May 2, 2019.
+
+import math
+#start = time.time()
+tot=0
+for i in range(1,1000000+1):
+    newlist = []
+    j = i
+    while(j not in newlist):
+        newlist.append(j)
+        j = sum(math.factorial(int(i)) for i in str(j))
+    if len(newlist)==60:
+        tot += 1
+#end = time.time()
+print(tot)        
