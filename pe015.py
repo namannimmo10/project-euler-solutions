674fc19f9266f928678f5a78d7da93d7173b6608
Naman Gera <43007653+NamanNimmo10@users.noreply.github.com>
2019-05-01 18:53:44 +0530

    Create pe015.py

---
+++ pe015.py
@@ -0,0 +1,9 @@
+#code created by NamanNimmo Gera
+#6:50pm, May 1, 2019.
+
+import math
+print(math.factorial(40)//(math.factorial(20)*math.factorial(20)))
+
+'''
+IT IS A SIMPLE PnC problem :), you just have to arrange 20 right moves and 20 down moves. 
+'''
