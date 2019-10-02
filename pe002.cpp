732e8f6ca5673210882d9ecfa569a728fb795d9f
Naman Gera <43007653+namannimmo10@users.noreply.github.com>
2019-10-02 10:44:22 +0530

    a solution in c++ for problem 2 also added

---
+++ pe002.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool odd(int i){
+   return i & 1;
+}
+int main () {
+   vector<int> fib(30);
+   fib[0] = 1;
+   adjacent_difference (fib.begin(), fib.end()-1,fib.begin()+1,
+                  std :: plus<int>());   
+   fib.erase (std :: remove_if (fib.begin(), fib.end(), odd),
+                     fib.end());
+   cout << accumulate (fib.begin(), fib.end(),
+                     0);
+   return 0;
+}
