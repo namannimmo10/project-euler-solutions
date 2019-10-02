4abd7ad2c5af3b2951822e51c719b7fc459bddd3
Naman Gera <43007653+namannimmo10@users.noreply.github.com>
2019-10-02 10:40:29 +0530

    A new solution for problem 33 added.

---
+++ pe033_2.py
@@ -0,0 +1,9 @@
+#approach no. 2 for solving problem 33
+import math
+frac=1.0
+for b in range(1,10):
+    for a in range(1,b):
+        for c in range(1,10):
+            if (a*10+b)/(b*10+c)==a/c:
+                frac*=(a/c)
+print(math.ceil(1/frac))
