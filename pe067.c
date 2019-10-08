a49beb4e4f71d99d2edecdccda19d35373cc76e6
BuzzHari <buzz.hari@gmail.com>
2019-10-08 10:12:24 +0530

    Solution to Problem 67 Added

---
+++ pe067.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+#define row 100 
+#define col 100 
+
+int main(){
+    int arr[101][101];
+
+    for(int i = 0; i < row; i++){
+        for(int j = 0; j<=i; j++){
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    
+    for(int i = row-1; i >= 90; i--){
+        for(int j = 0; j<=i; j++){
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    for(int i = row-2; i>=0; i--){
+        for(int j = 0; j<=i; j++){
+            if(arr[i][j]+arr[i+1][j] > arr[i][j] + arr[i+1][j+1])
+                arr[i][j] += arr[i+1][j];
+            else
+                arr[i][j] += arr[i+1][j+1];
+        }
+    }
+
+    printf("%d\n", arr[0][0]);
+
+    return 0;
+}
